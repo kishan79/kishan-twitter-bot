5c2644c5e4207b3122f022f80008259b115245be
kishan79 <kishan.237@gmail.com>
2018-09-02 02:30:08 +0530

    initial commit

---
+++ src/AutoDM.js
@@ -0,0 +1,48 @@
+const T = require("./Twit.js");
+const my_user_name = require("../config").userName;
+const timeout = 1000 * 60 * 5; 
+
+const AutoDM = () => {
+  const stream = T.stream("user");
+  console.log("Start Sending Auto Direct Message 🚀🚀🚀");
+  stream.on("follow", SendMessage);
+};
+
+const SendMessage = user => {
+  const { screen_name, name } = user.source;
+
+  const obj = {
+    screen_name,
+    text: GenerateMessage(name)
+  };
+ 
+  if (screen_name != my_user_name) {
+    console.log(" 🎉🎉🎉🎉 New Follower  🎉🎉🎉🎉🎉 ");
+    setTimeout(() => {
+      T.post("direct_messages/new", obj)
+        .catch(err => {
+          console.error("error", err.stack);
+        })
+        .then(result => {
+          console.log(`Message sent successfully To  ${screen_name}  💪💪`);
+        });
+    }, timeout);
+  }
+};
+const GenerateMessage = name => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  const d = new Date();
+  const dayName = days[d.getDay()];
+  return `Hi ${name} Thanks for being a part of my social media network. I'am the @Kishan797 Co-organiser of Google Developer Groups Jalandhar. I'm also a Flutter app developer.\n Happy to discuss anytime 😊 \n Happy ${dayName} 😊😊 `; 
+  
+};
+
+module.exports = AutoDM;
